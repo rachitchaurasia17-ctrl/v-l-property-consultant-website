6bd93485a354f05f697671c97b6075216ff82ae8
rachitchaurasia17-ctrl <rachitchaurasia17@gmail.com>
2026-06-11 22:36:52 +0530

    Fix: replace all template addresses with client address

--- components/PropertyAI.jsx
+++ components/PropertyAI.jsx
@@ -11,7 +11,7 @@ const SYSTEM_PROMPT = `You are Property AI, the digital concierge for V L Proper
 
 Firm facts:
 - Firm: V L Property Consultant
-- Office: SCO 1, 2nd Floor, Sector 79, Sahibzada Ajit Singh Nagar, Punjab 140308
+- Office: Commercial Hub, VIP Road, Zirakpur, Punjab
 - Phone: +91-00000-00000
 - GMADA empanelled, RERA registered (PB-RERA)
 - Practice: Residential, Commercial, Land/Plots, Investment Advisory, NRI Services, Title & Due Diligence
